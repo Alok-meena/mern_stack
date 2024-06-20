8aa881d0f118d47e91aee5f4d23c812503c5ca3b
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-06-20 22:18:41 +0530

    Update usefunction.js

--- nodejs/lecture1/usefunction.js
+++ nodejs/lecture1/usefunction.js
@@ -1,3 +1,15 @@
 const {greet}=require("./server3.js");
 
+const {add,subtract}=require("./math.js");
+
 console.log(greet("Alok"));
+
+console.log("2,4=",add(2,4));
+console.log("2-4=",subtract(2,4));t
+file accessed successfully
+
+o/p
+HELLO Alok
+2,4= 6
+2-4= -2
+
