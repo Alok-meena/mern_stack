19455c7c952863d265e10f9bb88591d3fe45bb20
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-06-28 15:54:02 +0530

    Create logger.js

---
+++ nodejs/lecture4/middleware/logger.js
@@ -0,0 +1,16 @@
+//MIDDLEWARE IS MOSTLY WRITTEN FOR AUTHENTICATION AND LOGIN 
+
+
+module.exports=(req,res,next)=>{
+    console.log("API----->>>",req.method,req.url);//it is req.url type of api means url return krega
+    //and req.method is used to console.log the api which is called
+    next();//mtlb ki middleware is completed and go ahead
+}
+
+//or if we want to export it like a function the do like this------>>>
+// const logger=()=>{
+//     console.log("API ---->>>",req.method,req.url);
+//     next();
+// }
+
+// module.exports={logger};
