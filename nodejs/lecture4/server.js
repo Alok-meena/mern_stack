132e59434ecea9c7698a6fe5693c7d1d904f9e95
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-06-28 15:51:03 +0530

    Update server.js

--- nodejs/lecture4/server.js
+++ nodejs/lecture4/server.js
@@ -1,1 +1,23 @@
-i
+
+const express=require("express");
+const app=express();
+//ye dono middleware hamne alag se file me likha because sabhi api me use krna hai
+const basicAuth=require("./auth.js");
+const logger=require("./logger.js");//it is used to display which api is called
+const port=3000;
+
+app.use(express.json());//ye to hamne postman pe data post ya get krne ke liye json format aa rha iske liye bheja tha
+app.use(logger);//it is to use logger in all api's
+
+app.get("/public",(req,res)=>{
+    res.send("THIS IS PUBLIC API");
+})
+
+//mtlb basicAuth middleware ko pass krne ke bad hi request access hogi
+app.get("/protected",basicAuth,(req,res)=>{
+    res.send("THIS IS PROTECTED API");
+})
+
+app.listen(port,()=>{
+    console.log("SERVER STARTED AT port:",port);
+})
