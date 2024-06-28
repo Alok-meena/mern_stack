131e39fafea2df8892863810743d30ddb55cfe29
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-06-28 15:54:37 +0530

    Update app.js

--- nodejs/lecture4/router/app.js
+++ nodejs/lecture4/router/app.js
@@ -1,1 +1,19 @@
-d
+ const express=require("express");
+ const app=express();
+ const port=3000;
+
+
+ //very imp ki ham ./ use kro jab path dena ho 
+ //and use ./ to go forward and use ./../ to go backward
+ const userRoutes=require("./routes/userRoutes");
+ const bookRoutes=require("./routes/bookRoutes");
+
+ app.use(express.json());
+
+app.use("/api/users",userRoutes);//ye router hoga tabhi uspe jayega apna pointer okk
+
+app.use("/api/books",bookRoutes);
+
+ app.listen(port,()=>{
+    console.log("SERVER IS RUNNING ON http://localhost:3000")
+ })
