f5e6315e63634410d6f28576e9d89e59a4d54bae
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-07-06 18:09:32 +0530

    Update auth.js

--- nodejs/lecture4/middleware/auth.js
+++ nodejs/lecture4/middleware/auth.js
@@ -5,7 +5,7 @@ module.exports=(req,res,next)=>{
     if(authHeader && authHeader.startsWith("Bearer")){
         const token=authHeader.split(" ")[1];//to ye xxxx ko utha lega mtlb space ke bad ke text ko
         if(token=="xxxx"){
-            return next();
+            return next();//to ager password correct hai to next() kiya so go in api logic
         }
     }
     res.status(401).send("WHO ARE YOU?");
