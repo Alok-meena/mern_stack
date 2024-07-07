2c1b20046c63f12bcf309ffe2760436c3c5130ce
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-07-07 13:30:56 +0530

    Update auth.js

--- nodejs/lecture4/middleware/auth.js
+++ nodejs/lecture4/middleware/auth.js
@@ -1,6 +1,7 @@
 //to isme ham function export krte hai to yha () arrow function return kr diya hai
 module.exports=(req,res,next)=>{
-    const authHeader=req.headers["authorization"];//req.headers kiya to include all headers
+    const authHeader=req.headers["authorization"];//req.headers kiya aor authorization ke value utha li hamne jo Bearer xxxx thi 
+    //ye same hai jaise ham pahle req.query.name/id/order etc krte the na bs
     console.log(authHeader);
     if(authHeader && authHeader.startsWith("Bearer")){
         const token=authHeader.split(" ")[1];//to ye xxxx ko utha lega mtlb space ke bad ke text ko
