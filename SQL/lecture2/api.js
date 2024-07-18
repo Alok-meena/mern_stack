b2967d9b5cbfe0a5fa06ceeb7cd2dbd4283a5970
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-07-19 00:16:25 +0530

    Update api.js

--- SQL/lecture2/api.js
+++ SQL/lecture2/api.js
@@ -1,5 +1,16 @@
 const express=require("express");
-const { createUser, createProduct, createOrder } = require("./functions");
+//so we also have to import the functions from the other file
+const { createUser,
+    createProduct, 
+    createOrder,
+    getUserById,
+    getAllUsers,
+    getAllProducts,
+    getAllOrders,
+    getOrderDetails,
+    updateProduct,
+    updateUser,
+   } = require("./functions");
 const router=express.Router();
 
 
@@ -42,24 +53,88 @@ router.post("/orders",async (req,res)=>{
 //api to get users data
 router.get("/users/:id",async (req,res)=>{
     try{
-        const id=parseInt(req.body.id);
-        const user=await getUserById(id);
+        // const id=parseInt(req.params.id);
+        const user=await getUserById(req.params.id);
+        res.json(user);
     }
     catch(err){
         console.log("Error in fething Users:",err);
     }
 })
 
-router.put("/users/:id",async (req,res)=>{
+router.get("/users",async (req,res)=>{
     try{
-        const updates=req.body;
-        const user=await updateUser(req.params.id,updates);
+        const users=await getAllUsers();
+        res.json(users);
+    }
+    catch(error){
+        console.log("Error fetching users:",error.stack);
+        res.status(500).send("Error fetching users");
+    }
+})
+
+router.get("/products",async (req,res)=>{
+    try{
+        const products=await getAllProducts();
+        res.json(products);
+    }
+    catch(error){
+        console.log("Error fetching users:",error.stack);
+        res.status(500).send("Error fetching users");
+    }
+})
+
+router.get("/orders",async (req,res)=>{
+    try{
+        const products=await getAllOrders();
+        res.json(products);
+    }
+    catch(error){
+        console.log("Error fetching users:",error.stack);
+        res.status(500).send("Error fetching users");
+    }
+})
+
+
+router.get("/orders/:id",async (req,res)=>{
+    try{
+        const user=await getOrderDetails(req.params.id);
+        res.json(user);
     }
     catch(err){
         console.log("Error updating user:",err.stack);
         res.status(500).send("Error updating user");
     }
 })
+
+
+
+router.put("/users/:id", async (req, res) => {
+  try {
+    const updates = req.body;
+    const user = await updateUser(req.params.id, updates);
+    res.json(user);
+  } catch (error) {
+    console.error("Error updating user:", error.stack);
+    res.status(500).send("Error updating user");
+  }
+});
+
+
+router.put("/products/:id", async (req, res) => {
+    try {
+      const updates = req.body;
+      const product = await updateProduct(req.params.id, updates);
+      res.json(product);
+    } catch (error) {
+      console.error("Error updating product:", error.stack);
+      res.status(500).send("Error updating product");
+    }
+  });
+
+
+
+
 
 module.exports=router;
 
