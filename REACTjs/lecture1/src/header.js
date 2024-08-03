9f8c9ec79372c143f9870aca1e2f20c9832662d0
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-03 12:52:53 +0530

    Add files via upload

---
+++ REACTjs/lecture1/src/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function header(){
+    return(
+        <header className="header">
+            <h1>This is a simple header</h1>
+        </header>
+    )
+}
+
+export default header;
