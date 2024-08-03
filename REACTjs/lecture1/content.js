9e3c749c563d08f3ab26f1702d9f7b4f1ee94d5a
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-03 12:49:53 +0530

    Add files via upload

---
+++ REACTjs/lecture1/content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./content.css";
+
+function content(){
+    return(
+        <main className="content">
+            <h1>Welcome to my simple React App</h1>
+        </main>
+    )
+}
+
+export default content;
