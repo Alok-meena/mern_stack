9d998e21e6b921aaf5309c5e8844923e7f14f639
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-05 19:19:59 +0530

    Update app.js

--- REACTjs/lecture2/app.js
+++ REACTjs/lecture2/app.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
-import Counter from "./components/Counter";
+// import React, { useState } from "react";
+// import Counter from "./components/Counter";
+
+// function App(){
+//     <div className="App">
+//         <Counter/>
+//     </div>
+// }
+
+// export default App;
+
+
+
+import React, { useState , useEffect } from "react";
+import Search from "./components/Search";
 
 function App(){
-    <div className="App">
-        <Counter/>
-    </div>
+    return(
+        <div className="App">
+          <Search/>
+        </div>
+    );
 }
 
 export default App;
+
