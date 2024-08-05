6ab3f44537002bc4c3b507a1044109adc41425f9
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-05 18:43:58 +0530

    Create app2.js

---
+++ REACTjs/lecture2/app.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter";
+
+function App(){
+    <div className="App">
+        <Counter/>
+    </div>
+}
+
+export default App;
