8fd72699834de560ce1dd9d6212e2628c2b4c067
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-10 15:26:14 +0530

    Update Counter.js

--- REACTjs/lecture2/components/Counter.js
+++ REACTjs/lecture2/components/Counter.js
@@ -16,3 +16,37 @@ const Counter = () => {
 }
 
 export default Counter;
+
+we can also do like this 
+
+    function App(){
+    const value=0;
+    const [counter,counterlogic] = useState(value);
+
+    const increment=()=>{
+        counterlogic(counter+1);
+    }
+
+    const decrement=()=>{
+        counterlogic(counter-1);//do it like this dont do it like counter--
+    }
+
+    const reset=()=>{
+        counterlogic(0);
+    }
+
+    return(
+        <div className="App">
+            <h1>{counter}</h1>
+           //yha ager only increment decremen likh doge to khuch nhi hoga 
+        //value change krne ke liye usi function me pass krna hota hai and then also call
+        //back function for calling increment function
+        
+            <button onClick={()=>counterlogic(increment)}>increment</button>
+            <button onClick={()=>counterlogic(decrement)}>decrement</button>
+            <button onClick={()=>counterlogic(reset)}>reset</button>
+        </div>
+    )
+}
+
+export default App;
