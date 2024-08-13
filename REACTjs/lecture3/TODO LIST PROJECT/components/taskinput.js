9031247f527f685197e37e166a6fd4a6a398088e
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-13 21:56:57 +0530

    Update taskinput.js

--- REACTjs/lecture3/TODO LIST PROJECT/components/taskinput.js
+++ REACTjs/lecture3/TODO LIST PROJECT/components/taskinput.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+
+
+//Object-like Access: When props are passed to a component, they are available as an object, which is why you can use object destructuring syntax { addTask } to access the addTask prop directly. 
+//This is a feature of JavaScript ES6 and is not specific to React.
 const TaskInput = ({addTask})=>{
     const [task,setTask]=useState("");
     const handleSubmit = (e) =>{
