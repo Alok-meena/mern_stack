89a8e7a797fd9d99d2c20a3833263c371af76fde
Alok kumar meena <123947718+Alok-meena@users.noreply.github.com>
2024-08-13 21:32:52 +0530

    Update app.js

--- REACTjs/lecture3/TODO LIST PROJECT/components/app.js
+++ REACTjs/lecture3/TODO LIST PROJECT/components/app.js
@@ -20,7 +20,8 @@ const App = () =>{
     return(
         <div className="App">
             <h1>ToDo List</h1>
-            <TaskInput addTask={addTask}/>
+            <TaskInput addTask={addTask}/> // to dekho yha se ham bhej rhe hai props okk aor vha ham props krke bhi access kr skte hai aor ye object ki tarah pass
+            //hota hai to {addTask} krke bhi access kr skte hai isko ham
             <TaskList tasks={tasks} deleteTask={deleteTask}/>
         </div>
     )
